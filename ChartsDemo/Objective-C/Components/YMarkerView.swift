710195ec49c787bf1efc46d526e01d0414d3d5e7
fanrongzeng <zengfanrong@icarbonx.com>
2018-01-20 17:21:16 +0800

    添加修改过的Mark到工程中

---
+++ ChartsDemo/Objective-C/Components/YMarkerView.swift
@@ -0,0 +1,47 @@
+//
+//  YMarkerView.swift
+//  Meum
+//
+//  Created by fanrong on 2017/11/28.
+//  Copyright © 2017年 huangwei. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+open class YMarkerView: BalloonMarker
+{
+    fileprivate var yFormatter = NumberFormatter()
+    
+    @objc public var digits:Int {
+        set{
+            yFormatter.minimumFractionDigits = newValue
+            yFormatter.maximumFractionDigits = newValue
+        }
+        get {
+           return yFormatter.minimumFractionDigits
+        }
+    }
+    
+    @objc public init(color: UIColor, font: UIFont, textColor: UIColor, insets: UIEdgeInsets,
+                      xAxisValueFormatter: IAxisValueFormatter)
+    {
+        super.init(color: color, font: font, textColor: textColor, insets: insets)
+        yFormatter.minimumFractionDigits = 0
+        yFormatter.maximumFractionDigits = 0
+    }
+    
+    open override func refreshContent(entry: ChartDataEntry, highlight: Highlight)
+    {
+        if entry.y == 0.0 {
+             setLabel("")
+        }else {
+            let string = yFormatter.string(from: NSNumber(value: entry.y))
+            if let string = string,string.count > 0 {
+                setLabel(string)
+            }else {
+                setLabel("")
+            }
+        }
+    }
+}
